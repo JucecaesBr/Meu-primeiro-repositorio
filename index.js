ce319fb4d110d39d3989ac865924daa678c7a423
Jucecaes <146789353+JucecaesBr@users.noreply.github.com>
2023-10-18 22:52:56 +0000

    Atualizaçao

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Produto = mongoose.model ('Produto',
+{
+    CodigoDeBarras: String,
+    nome:String,
+    preco: Number
+});
+
+const Produto1= new Produto(
+{
+    CodigoDebarras: '912873987LC',
+    nome:"arroz",
+    preco: 10
+})
+Produto1.save().then(console.log('Produto Registrado'))
